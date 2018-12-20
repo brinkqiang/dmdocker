e89b99040df03d1b6a9f4cf9e9dbcad8b7d609f7
brinkqiang <brink.qiang@gmail.com>
2018-12-20 21:57:13 +0800

    add

--- dk.sh
+++ dk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+sudo docker rmi -f dmgen-test:v1
 sudo docker build -t dmgen-test:v1 .
 sudo docker rmi -f $(sudo docker images -f "dangling=true" -q)
 sudo docker run -it dmgen-test:v1 /bin/bash
