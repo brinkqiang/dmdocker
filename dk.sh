d556e61cfc39fa35f85bf157cbc1e52634a323e8
brinkqiang <brink.qiang@gmail.com>
2018-12-22 10:19:18 +0800

    add

--- dk.sh
+++ dk.sh
@@ -4,5 +4,4 @@ sudo service docker restart
 sudo docker pull centos
 sudo docker rmi -f dmtimer-test:v1
 sudo docker build -t dmtimer-test:v1 .
-sudo docker rmi -f $(sudo docker images -f "dangling=true" -q)
 sudo docker run -it dmtimer-test:v1
