35bfc63755f15a519dbb3ccb315bf83a8e05552b
brinkqiang <brink.qiang@gmail.com>
2020-01-16 09:44:59 +0800

    add

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,7 @@ FROM centos:latest
 MAINTAINER brinkqiang <brink.qiang@gmail.com>
 LABEL name="dmgen Image"
 LABEL vendor="brinkqiang"
-RUN yum -y install git install cmake gcc gcc-c++ autoconf libtool automake make && cd home && mkdir myapp && cd myapp && git clone --progress --recursive -v "https://github.com/brinkqiang/dmtimer" && cd dmtimer && sh build.sh && cd ..
-CMD pwd && cd /home/myapp/dmtimer/bin && ./dmtimertest
+RUN yum -y install git install cmake gcc gcc-c++ autoconf libtool automake make 
+RUN mkdir -p /home/myapp && cd /home/myapp
+RUN git clone --progress --recursive -v "https://github.com/brinkqiang/dmtimer" && cd dmtimer && bash build.sh && cd ..
+CMD pwd && cd /home/myapp/dmtimer/bin/relwithdebinfo && ./dmtimertest
--- dk.sh
+++ dk.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
-sudo yum -y install docker
-sudo service docker restart
+
+if [ -f /etc/redhat-release ]; then
+    sudo yum -y install yum-utils
+    sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+    sudo yum clean all && yum makecache
+    sudo yum -y install docker-ce
+    sudo systemctl restart docker
+    sudo yum clean all 
+    sudo yum makecache
+fi
+
+if [ -f /etc/lsb-release ]; then
+    curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt -y update
+    sudo apt -y install docker-ce
+    sudo systemctl restart docker
+fi
+
 sudo docker pull centos
 sudo docker rmi -f dmtimer-test:v1
 sudo docker build -t dmtimer-test:v1 .
